630439bc7ace5596978dd94764c0baf8cbfb27da
suryafakhriy <55834014+suryafakhriy@users.noreply.github.com>
2019-10-10 21:45:31 +0700

    Add files via upload

---
+++ IF-01-10118028-Latihan18-Kelipatan/src/pkgif/pkg01/pkg10118028/latihan18/kelipatan/IF0110118028Latihan18Kelipatan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif.pkg01.pkg10118028.latihan18.kelipatan;
+
+/**
+ *
+ * @author User
+ * NAMA      : Surya Fakhriy Hasbi
+   KELAS     : IF-01 2018
+   NIM       : 10118028
+   DESKRIPSI : Kelipatan
+ */
+public class IF0110118028Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double i = 3.5;
+        while(i <= 35){
+            System.out.println(i);
+            i += 3.5;
+        }
+        System.out.println("(Developed by : Surya Fakhriy Hasbi)");
+    }
+    
+}
